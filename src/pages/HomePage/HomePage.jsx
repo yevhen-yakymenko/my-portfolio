5c9c0a3711fb4218753e0c9327fee2e1b8f3e617
Yevhen <yevhen.yakymenko.fed@gmail.com>
2023-01-19 23:12:25 +0200

    corrected img url

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -56,7 +56,7 @@ const HomePage = () => {
           <MediaBox>
             <ImgWrapper>
               <img
-                src="https://res.cloudinary.com/djjgaui2x/image/upload/v1674159865/1x3_euzh9w.jpg"
+                src="https://res.cloudinary.com/djjgaui2x/image/upload/v1674159865/my-portfolio/home-page/1x3_euzh9w.jpg"
                 alt="I"
                 width="400px"
               />
